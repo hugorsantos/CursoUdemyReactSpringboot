7037b9f32f2d480b8e385afe85bb90d7bd581922
Hugo <hugorsantos@gmail.com>
2021-10-01 10:31:59 -0300

    Atualizações

---
+++ financas/src/main/java/br/com/hrsystem/financas/model/entity/Lancamento.java
@@ -0,0 +1,36 @@
+package br.com.hrsystem.financas.model.entity;
+
+import org.springframework.data.convert.Jsr310Converters;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "lancamento", schema = "financas")
+public class Lancamento {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mes")
+    private Integer mes;
+
+    @Column(name = "ano")
+    private Integer ano;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @Column(name = "valor")
+    private BigDecimal valor;
+
+    @Column(name = "data_cadastro")
+    @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+    private LocalDate dataCadastro;
+
+}
